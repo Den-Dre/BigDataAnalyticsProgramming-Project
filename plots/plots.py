6884c682f438acef3b5f6684594b4b92b9c096e4
Andreas Hinderyckx <andreas.hinderyckx@student.kuleuven.be>
2021-12-16 09:13:25 +0100

    Add & incorporate savedPlots directory

--- plots/plots.py
+++ plots/plots.py
@@ -4,12 +4,13 @@ import argparse
 from glob import glob
 
 parser = argparse.ArgumentParser()
-choices = [c.split('/')[-1] for c in glob(r'../code/*.acc')]
+choices = ['\n' + c for c in glob(r'../code/savedOutputs/*.acc')]
 parser.add_argument('data_file_path', help='The name of the .csv file to plot', choices=choices, type=str)
 args = parser.parse_args()
 
 names = ['Number of examples', 'Accuracy']
-df = pd.read_csv(f'../code/{args.data_file_path}', index_col=0, names=names,  delim_whitespace=True)
+plt.locator_params(axis='x', nbins=10)
+df = pd.read_csv(f'{args.data_file_path}', index_col=0, names=names,  delim_whitespace=True)
 # df[names[1]] *= 100
 # print(df.head())
 df.plot(xlabel='Number of examples trained with', ylabel='Accuracy')

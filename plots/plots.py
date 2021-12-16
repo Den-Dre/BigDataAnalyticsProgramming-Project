054deb0e754aaf5ac209774d3da5744ab61596ee
Andreas <ahinderyckx@yahoo.co.uk>
2021-12-16 09:12:48 +0100

    Update plotter

--- plots/plots.py
+++ plots/plots.py
@@ -4,7 +4,7 @@ import argparse
 from glob import glob
 
 parser = argparse.ArgumentParser()
-choices = [c.split('/')[-1] for c in glob(r'../code/*.acc')]
+choices = ['\n' + c for c in glob(r'../code/savedOutputs/*.acc')]
 parser.add_argument('data_file_path', help='The name of the .csv file to plot', choices=choices, type=str)
 args = parser.parse_args()
 
@@ -12,5 +12,10 @@ names = ['Number of examples', 'Accuracy']
 df = pd.read_csv(f'../code/{args.data_file_path}', index_col=0, names=names,  delim_whitespace=True)
 # df[names[1]] *= 100
 # print(df.head())
-df.plot(xlabel='Number of examples trained with', ylabel='Accuracy')
+
+step = 10
+plt.plot(df.index[::step], df.iloc[::step], linewidth=0.5)
+# df.plot(xlabel='Number of examples trained with', ylabel='Accuracy')
+plt.xlabel('Number of examples trained with')
+plt.ylabel('Accuracy')
 plt.show()
